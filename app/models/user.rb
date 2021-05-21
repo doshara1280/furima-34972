4cfbf30c4c29b65584a0f28b0c311645e7466986
doshara1280 <danyuan749xa@gmail.com>
2021-05-21 21:16:10 +0900

    名前に関するバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :last_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]/ }
+  validates :last_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+  validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
   validates :birthday, presence: true
 end
