2f7f33f8d00b4806cefe1e2295f4ecd47451d406
doshara1280 <danyuan749xa@gmail.com>
2021-05-21 21:17:10 +0900

    passwordに英数字両方が確実に含まれるよう修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.initials }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password }
+    password { '1a' + Faker::Internet.password }
     password_confirmation { password }
     last_name_kanji { Faker::Japanese::Name.last_name }
     first_name_kanji { Faker::Japanese::Name.first_name }
