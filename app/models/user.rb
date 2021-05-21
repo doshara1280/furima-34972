221aa9baa5e5fb7a703a05bc3a2ce1d95c0b07c7
doshara1280 <danyuan749xa@gmail.com>
2021-05-21 17:56:47 +0900

    テストコードで使用するgemの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :last_name_kanji, presence: true
-  validates :first_name_kanji, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  validates :last_name_kanji, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name_kanji, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ン]/}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ン]/}
   validates :birthday, presence: true
 end
