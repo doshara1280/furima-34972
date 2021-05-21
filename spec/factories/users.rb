858fd98b3beb206996c6f420303a308975764059
doshara1280 <danyuan749xa@gmail.com>
2021-05-21 17:58:18 +0900

    仮値を使用するためのFakerを実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.initials}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password}
+    password_confirmation {password}
+    last_name_kanji {Faker::Japanese::Name.last_name}
+    first_name_kanji {Faker::Japanese::Name.first_name}
+    last_name_kana {Faker::Japanese::Name.last_name.yomi}
+    first_name_kana {Faker::Japanese::Name.first_name.yomi}
+    birthday {Faker::Date.in_date_period}
+  end
+end
