3a8b1f9161a7614486e146b6e77917981bcfd3d3
doshara1280 <danyuan749xa@gmail.com>
2021-05-22 12:26:38 +0900

    faker使わず文字列指定(偶に「ー」含む日本語らしからぬ名前が入力される）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,8 +6,8 @@ FactoryBot.define do
     password_confirmation { password }
     last_name_kanji { Faker::Japanese::Name.last_name }
     first_name_kanji { Faker::Japanese::Name.first_name }
-    last_name_kana { Faker::Japanese::Name.last_name.yomi }
-    first_name_kana { Faker::Japanese::Name.first_name.yomi }
+    last_name_kana { 'ヤス' }
+    first_name_kana { 'ヒロ' }
     birthday { Faker::Date.in_date_period }
   end
 end
