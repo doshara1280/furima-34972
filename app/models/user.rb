fa9614eca3e5af7f7e3f171ca4080da288947a27
doshara1280 <danyuan749xa@gmail.com>
2021-05-22 12:24:51 +0900

    with_optionsでバリデーションの可読性向上

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  # validates :nickname, presence: true
+  # validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  # validates :last_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  # validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  # validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+  # validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+  # validates :birthday, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :last_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-  validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ } do
+      validates :last_name_kanji
+      validates :first_name_kanji
+    end
+
+    with_options format: { with: /\A[ァ-ン]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
 end
