1a844850571bb8008a6b9944cb47158eacc6df1e
doshara1280 <danyuan749xa@gmail.com>
2021-05-26 20:31:13 +0900

    商品出品用のアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shipping_fee_burden_id,
+                                 :shipping_area_id, :day_to_ship_id, :price).merge(user_id: current_user.id)
+  end
 end
