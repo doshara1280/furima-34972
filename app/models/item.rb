867c54dc961f8e9fe03d1d911614bac761f1ee69
doshara1280 <danyuan749xa@gmail.com>
2021-05-26 20:29:15 +0900

    エラーメッセージの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,9 +12,8 @@ class Item < ApplicationRecord
     validates :image
     validates :name, length: { maximum: 40 }
     validates :description, length: { maximum: 1000 }
-    validates :price, numericality: { in: 300..9_999_999 }
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-    with_options numericality: { other_than: 1 } do
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
       validates :category_id
       validates :status_id
       validates :shipping_fee_burden_id
