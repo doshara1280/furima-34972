e3536509c66882550b397666854b52ffa4548659
doshara1280 <danyuan749xa@gmail.com>
2021-06-06 10:51:29 +0900

    ユーザー登録テストコード実行時の入力値修正(エラー回避)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :user do
-    nickname { Faker::Name.initials }
+    nickname { 'yasu' }
     email { Faker::Internet.free_email }
     password { '1a' + Faker::Internet.password }
     password_confirmation { password }
-    last_name_kanji { Faker::Japanese::Name.last_name }
-    first_name_kanji { Faker::Japanese::Name.first_name }
+    last_name_kanji { '安' }
+    first_name_kanji { '広' }
     last_name_kana { 'ヤス' }
     first_name_kana { 'ヒロ' }
     birthday { Faker::Date.in_date_period }
