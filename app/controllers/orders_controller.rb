9321ebcd45c07cbc49863baf862cbb54092f3aee
doshara1280 <danyuan749xa@gmail.com>
2021-06-11 08:13:07 +0900

    商品購入ページのパスを直接指定した場合、商品が売却済みならトップページへ遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,14 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :item_params_get, only: [:index, :create]
+  before_action :move_to_top, only: [:index]
+
   def index
     @orders = Order.new
-    @item = Item.find(params[:item_id])
     @purchase_history_order = PurchaseHistoryOrder.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order = Order.new
     @purchase_history_order = PurchaseHistoryOrder.new(purchase_history_order_params)
     if @purchase_history_order.valid?
@@ -25,9 +26,18 @@ class OrdersController < ApplicationController
     params.require(:purchase_history_order).permit(:postal_code, :shipping_area_id, :municipality, :address, :building_name, :tel).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
+  def item_params_get
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_top  
+    if current_user.id == @item.user.id
+      redirect_to root_path
+    end
+  end
+
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    binding.pry
     Payjp::Charge.create(
       amount: @item.price,
       card: purchase_history_order_params[:token],
